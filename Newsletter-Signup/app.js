e223145ebbd702762fe87a83ef368f6d81058c10
rohullahN <41601768+rohullahN@users.noreply.github.com>
2022-01-31 20:46:44 -0500

    added procfile for heroku deployment

--- Newsletter-Signup/app.js
+++ Newsletter-Signup/app.js
@@ -36,7 +36,7 @@ app.post("/", function(req, res){
   const url = "https://us14.api.mailchimp.com/3.0/lists/d4e3ac2e08";
   const options = {
     method: "POST",
-    auth: "rohullah1:d235198fede4aff6b6855be74fc6fbe6-us14"
+    auth: "rohullah1:ad235198fede4aff6b6855be74fc6fbe6-us14"
   }
 
   const request = https.request(url, options, function(response){
@@ -46,18 +46,21 @@ app.post("/", function(req, res){
       res.sendFile(__dirname+"/success.html");
     }
     else{
-      res.sendFile(__dirname+"/fail.html");
+      res.sendFile(__dirname+"/fail.html")
     }
-    response.on("data", function(data){
-      console.log(JSON.parse(data));
-    })
   })
 
-request.write(jsonData);
-request.end();
+  request.write(jsonData);
+  request.end();
 
 });
-app.listen(3000, function()
+
+app.post("/fail", function(req, res){
+  res.redirect("/");
+})
+
+
+app.listen(process.env.PORT || 3000, function()
 {
   console.log("server is running on port 3000");
 })
